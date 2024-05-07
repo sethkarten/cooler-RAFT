bd4068eb819023183af1ec327f7a89255ac1b828
Elizabeth Mieczkowski <bethmiecz@gmail.com>
2024-05-06 22:47:46 -0400

    client intervals

--- run_experiments.sh
+++ run_experiments.sh
@@ -11,6 +11,7 @@ latencies=(0 2 8)
 timeouts=(1 10 40 100)
 max_failures=(1 4 10 14) 
 failure_intervals=(0 2 8 32)
+client_intervals=(1 5 10)
 
 output_dir="./outputs/experiment_logs"
 mkdir -p $output_dir
@@ -20,14 +21,16 @@ for nodes in "${num_nodes[@]}"; do
         for timeout in "${timeouts[@]}"; do
             for failure in "${max_failures[@]}"; do
                 for fail_interval in "${failure_intervals[@]}"; do
-                    if (( failure < ((nodes+1)/2) )); then
-                        log_filename="${output_dir}/log_n${nodes}_l${latency}_t${timeout}_f${failure}_i${fail_interval}/"
-                        mkdir -p $log_filename
-                        echo "Running: ${nodes} nodes, ${latency}ms latency, ${timeout}s timeout, ${failure} failures, ${fail_interval}s fail interval"
-                        python3 main.py --num_nodes=$nodes --interval=$timeout --filepath="$log_filename" --latency=$latency --max_failures=$failure --failure_interval=$fail_interval
-                        echo "Done"
-                        sleep 5
-                    fi
+                    for client_interval in "${client_intervals[@]}"; do
+                        if (( failure < ((nodes+1)/2) )); then
+                            log_filename="${output_dir}/log_n${nodes}_l${latency}_t${timeout}_f${failure}_i${fail_interval}/"
+                            mkdir -p $log_filename
+                            echo "Running: ${nodes} nodes, ${latency}ms latency, ${timeout}s timeout, ${failure} failures, ${fail_interval}s fail interval"
+                            python3 main.py --num_nodes=$nodes --interval=$timeout --filepath="$log_filename" --latency=$latency --max_failures=$failure --failure_interval=$fail_interval --client_interval=$client_interval
+                            echo "Done"
+                            sleep 5
+                        fi
+                    done
                 done
             done
         done
