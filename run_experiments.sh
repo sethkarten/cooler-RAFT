701f6890d24a2fac6cd06ac7a3d21bc0b56de99e
Seth <sethkarten@princeton.edu>
2024-05-06 22:13:31 -0400

    change log directory

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,7 +3,7 @@
 num_nodes=(3 10 30)
 latencies=(0 2 8)
 timeouts=(1 10 40 100)
-max_failures=(1 4 10 14 20 24 30)  
+max_failures=(1 4 10 14)  
 failure_intervals=(0 2 8 32)
 
 output_dir="./outputs/experiment_logs"
@@ -15,7 +15,8 @@ for nodes in "${num_nodes[@]}"; do
             for failure in "${max_failures[@]}"; do
                 for fail_interval in "${failure_intervals[@]}"; do
                     if (( failure < ((nodes+1)/2) )); then
-                        log_filename="${output_dir}/log_n${nodes}_l${latency}_t${timeout}_f${failure}_i${fail_interval}.txt"
+                        log_filename="${output_dir}_log_n${nodes}_l${latency}_t${timeout}_f${failure}_i${fail_interval}"
+                        mkdir -p $log_filename
                         echo "Running: ${nodes} nodes, ${latency}ms latency, ${timeout}s timeout, ${failure} failures, ${fail_interval}s fail interval"
                         python3 main.py --num_nodes=$nodes --interval=$timeout --filepath="$log_filename" --latency=$latency --max_failures=$failure --failure_interval=$fail_interval
                         echo "Done"
