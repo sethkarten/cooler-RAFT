24c1171804c085926cb91a98931ab3347752e607
Elizabeth Mieczkowski <bethmiecz@gmail.com>
2024-05-07 11:01:27 -0400

    added client interval

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-# num_nodes=(3)
-# latencies=(0)
-# timeouts=(1)
-# max_failures=(1)  
-# failure_intervals=(0)
-
 num_nodes=(3 10 30)
-latencies=(0 2 8)
-timeouts=(1 10 40 100)
-max_failures=(1 4 10 14) 
-failure_intervals=(0 2 8 32)
-client_intervals=(1 5 10)
+# latencies=(0 2 8)
+latencies=(2)
+# timeouts=(1 10 40 100)
+timeouts=(1 40 100)
+# max_failures=(1 4 10 14) 
+max_failures=(1 4 14) 
+# failure_intervals=(0 2 8 32)
+failure_intervals=(8)
+# client_intervals=(1 5 10)
+client_intervals=(5)
 counter=9000
+
+max_jobs=9
 
 output_dir="./outputs/experiment_logs"
 mkdir -p $output_dir
 
+active_jobs=0
 for nodes in "${num_nodes[@]}"; do
     for latency in "${latencies[@]}"; do
         for timeout in "${timeouts[@]}"; do
@@ -24,13 +26,18 @@ for nodes in "${num_nodes[@]}"; do
                 for fail_interval in "${failure_intervals[@]}"; do
                     for client_interval in "${client_intervals[@]}"; do
                         if (( failure < ((nodes+1)/2) )); then
-                            log_filename="${output_dir}/log_n${nodes}_l${latency}_t${timeout}_f${failure}_i${fail_interval}/"
+                            log_filename="${output_dir}/log_n${nodes}_l${latency}_t${timeout}_f${failure}_i${fail_interval}_i${client_interval}/"
                             mkdir -p $log_filename
                             echo "Running: ${nodes} nodes, ${latency}ms latency, ${timeout}s timeout, ${failure} failures, ${fail_interval}s fail interval , ${client_interval}s client interval"
                             python3 main.py --base_port=$counter --num_nodes=$nodes --interval=$timeout --filepath="$log_filename" --latency=$latency --max_failures=$failure --failure_interval=$fail_interval --client_interval=$client_interval &
                             # echo "Done"
                             ((counter+=100))
-                            sleep 5
+                            # sleep 5
+                            ((active_jobs++))
+                            if (( active_jobs >= max_jobs )); then
+                                wait
+                                ((active_jobs--))
+                            fi
                         fi
                     done
                 done
@@ -38,3 +45,4 @@ for nodes in "${num_nodes[@]}"; do
         done
     done
 done
+wait
